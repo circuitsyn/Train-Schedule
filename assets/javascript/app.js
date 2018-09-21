ebdaf7c34e7929559a40ed05a78ffae5cc23e792
CircuitSyn <circuitsyn@gmail.com>
2018-09-21 16:28:50 -0700

    initial setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD4GLTMegSYCW7DkZ6knl6wI_jLuaDRaqk",
+    authDomain: "train-schedule-45898.firebaseapp.com",
+    databaseURL: "https://train-schedule-45898.firebaseio.com",
+    projectId: "train-schedule-45898",
+    storageBucket: "",
+    messagingSenderId: "643061639145"
+  };
+  firebase.initializeApp(config);
+
+});
